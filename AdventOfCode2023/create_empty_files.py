8fc79257c815e185f8214b05a8dcca97a60da104
wbiesmans <wbiesmans@goodix.com>
2023-12-05 12:09:32 +0100

    Wouter Day 5 pt 1 - Object oriented

--- AdventOfCode2023/create_empty_files.py
+++ AdventOfCode2023/create_empty_files.py
@@ -1,17 +1,17 @@
 if __name__  == "__main__":
     person = "Wouter"  # Used as subfolder name
-    days = range(4,5) # Days to create files for. E.g. range(1, 26) for all days
+    days = range(5,6) # Days to create files for. E.g. range(1, 26) for all days
     extension = ".py"
 
     for day in days:
         # create empty python files for each part
-        with open(f"{person}\day_{day}_part_1{extension}", "w") as f:
+        with open(f"{person}/day_{day}_part_1{extension}", "w") as f:
             pass
-        with open(f"{person}\day_{day}_part_2{extension}", "w") as f:
+        with open(f"{person}/day_{day}_part_2{extension}", "w") as f:
             pass
 
         # create empty input files
-        with open(f"{person}\day_{day}_input_test.txt", "w") as f:
+        with open(f"{person}/day_{day}_input_test.txt", "w") as f:
             pass
-        with open(f"{person}\day_{day}_input_final.txt", "w") as f:
+        with open(f"{person}/day_{day}_input_final.txt", "w") as f:
             pass
